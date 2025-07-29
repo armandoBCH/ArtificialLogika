411f2a6941d1e68f90860a6434908287a472d241
Armando Beato Chang <armadobeatochang@gmail.com>
2025-07-29 00:11:48 -0300

    d

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Landing Page de Artificial Lógika (Copy)/App';
-import './Landing Page de Artificial Lógika (Copy)/styles/globals.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
